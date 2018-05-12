dda028f77e3995a08ac22cfc9ea79ee4597e21e0
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-12 11:14:20 -0300

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_135046) do
+ActiveRecord::Schema.define(version: 2018_05_12_140937) do
 
   create_table "matchdays", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "code"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2018_05_12_135046) do
     t.string "name"
     t.string "fifa_code"
     t.string "flag_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name "John Doe"
+    username "johndoe"
+    email "john@doe.com"
+    avatar "http://photos.url/johndoe"
+  end
+end
