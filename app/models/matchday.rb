481943a7eba69be9379a0a467187a84e8e1afbe1
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-12 18:49:54 -0300

    Added validations to predictions

--- app/models/matchday.rb
+++ app/models/matchday.rb
@@ -4,4 +4,8 @@ class Matchday < ApplicationRecord
   scope :available, -> do
     where('started_at < :now AND ended_at > :now', now: DateTime.current)
   end
+
+  def available?
+    -> (date) { started_at < date && ended_at > date }.call(DateTime.current)
+  end
 end
--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,4 +1,11 @@
 class Prediction < ApplicationRecord
   belongs_to :user
   belongs_to :match
+
+  validate :matchday_is_available, if: -> { home_score_changed? || away_score_changed? }
+
+  private
+  def matchday_is_available
+    match.matchday.available?
+  end
 end
