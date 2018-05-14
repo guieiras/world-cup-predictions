2d082e548c6954a3b78235f11aa29bcf71d5317d
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-13 22:32:12 -0300

    Added Matchday updating

--- app/controllers/admin/matchdays_controller.rb
+++ app/controllers/admin/matchdays_controller.rb
@@ -8,4 +8,11 @@ class Admin::MatchdaysController < AdminController
     @matches = Match.includes(:home_team, :away_team, :stadium).where(matchday: @matchday)
     @teams = Team.all
   end
+
+  def update
+    @matchday = Matchday.find(params[:id])
+    @matchday.update(params.require(:matchday).permit(:started_at, :ended_at))
+
+    redirect_to action: "index"
+  end
 end
