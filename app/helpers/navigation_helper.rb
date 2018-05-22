4fdce1bdba0f68b74142a68c586271041cde8b69
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-21 21:36:41 -0300

    Added Submenu

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -11,4 +11,23 @@ module NavigationHelper
       end
     end
   end
+
+  def submenu_for options, icon:, text:
+    active = false
+    active = true if options[:controller] && controller_name == options[:controller]
+    active = true if options[:path] && current_page?(options)
+
+    content_tag(:li, class: "treeview #{'menu-open' if active}") do
+      (link_to '#' do
+        content_tag('i', '', class: "fa fa-#{icon}") +
+        content_tag('span', text) +
+        content_tag('span', class: "pull-right-container") do
+          content_tag('i', '', class: "fa fa-angle-left pull-right")
+        end
+      end) +
+      content_tag('ul', class: "treeview-menu", style: "#{'display: block;' if active}") do
+        yield
+      end
+    end
+  end
 end
