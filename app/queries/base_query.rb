452b702f9add10e2e39e3e4ad5e539cb1d970108
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-25 18:26:30 -0300

    Added Base Query

---
+++ app/queries/base_query.rb
@@ -0,0 +1,30 @@
+class BaseQuery
+  attr_reader :payload
+
+  def self.execute(payload)
+    new(payload).execute
+  end
+
+  def initialize(payload)
+    @payload = OpenStruct.new(payload)
+  end
+
+  def build
+    raise '#build must be implemented on BaseQuery children'
+  end
+
+  def schema
+    nil
+  end
+
+  def execute
+    rows = ActiveRecord::Base.connection.execute(build)
+    if schema
+      rows.map do |row|
+        OpenStruct.new(row.map.with_index { |column, idx| [schema[idx], column] }.to_h)
+      end
+    else
+      rows
+    end
+  end
+end
