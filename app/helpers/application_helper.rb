3e1b75bc8817005db9953add870c997cda5d52f3
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-28 20:21:16 -0300

    Added Flash helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
 module ApplicationHelper
+  include FlashHelper
   include NavigationHelper
 end
---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,14 @@
+module FlashHelper
+  def flash_notification_script
+    flashes = []
+    [:alert, :info, :success, :danger].each do |type|
+      flashes << [type, flash[type]] if flash[type].present?
+    end
+
+    if flashes.any?
+      script = flashes.reduce('') { |memo, f| memo + "toastr.#{f[0]}('#{j(f[1])}');" }
+
+      "<script>$(document).ready(function() { #{script} })</script>".html_safe
+    end
+  end
+end
