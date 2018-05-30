ce4d8e2f06cda92b0d10cd44b8eb20f7f582b3d6
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-29 23:51:20 -0300

    Fixed mailcatcher port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "mailcatcher", :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => "mailcatcher", :port => 25 }
   config.action_mailer.default_url_options = { host: ENV['EMAIL_HOST'] }
 
   # Print deprecation notices to the Rails logger.
