f3b9ceadfda6f37286c84fb9333a005ac4301301
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-05-31 21:40:56 -0300

    Added Leaderboard History

--- app/reports/league_report.rb
+++ app/reports/league_report.rb
@@ -29,6 +29,38 @@ class LeagueReport
     end
   end
 
+  def leaderboard_history
+    @history ||= -> {
+      labels = matches.where(finished: true)
+                .to_a.pluck(:id, :datetime)
+                .inject({}) do |memo, match|
+                  date = I18n.l(match[1].to_date)
+                  memo[date] ||= []
+                  memo[date] << match[0].to_s
+                  memo
+                end
+      results = query.map do |player|
+        { name: player.player_name,
+          dataset: player.results.to_a.map do |result|
+                      [result[0], result[1]["score"]]
+                    end.to_h
+        }
+      end
+
+      {
+        labels: labels.keys,
+        datasets: results.map do |player|
+          {
+            label: player[:name],
+            data: labels.values.map do |date|
+              date.map { |match_id| player[:dataset][match_id] }.sum
+            end
+          }
+        end
+      }
+    }.call
+  end
+
   private
   def query
     @query ||= LeaderboardQuery.execute(league_id: @league.id).map do |player|
