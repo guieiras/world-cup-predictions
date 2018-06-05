de87006b373bbc0b135c3aec9a298e0be07fdacc
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-06-05 00:26:12 -0300

    Configuring Email Host in Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "workspace_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: ENV.fetch('EMAIL_HOST') }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
