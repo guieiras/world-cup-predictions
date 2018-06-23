9c41a47e32547b49fb0952cdf7d761246961bae4
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-06-22 23:10:29 -0300

    Improved BaseQuery to allow raw requests

--- app/queries/base_query.rb
+++ app/queries/base_query.rb
@@ -17,8 +17,12 @@ class BaseQuery
     nil
   end
 
+  def raw_execute
+    ActiveRecord::Base.connection.execute(build)
+  end
+
   def execute
-    rows = ActiveRecord::Base.connection.execute(build)
+    rows = raw_execute
     if schema
       rows.map do |row|
         OpenStruct.new(row.map.with_index { |column, idx| [schema[idx], column[1]] }.to_h)
